964e865206374f76cc1967c67b34c7a7d9605e6b
ManUtopiK <emmanuel.salomon@gmail.com>
2020-01-27 00:20:55 +0100

    Configure PWA

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,32 @@
 module.exports = {
   theme: 'yuu',
-  plugins: [['vuepress-plugin-code-copy', {
-    color: '#3eaf7c',
-    backgroundColor: '#3eaf7c'
-  }]],
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/images/icons/icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
+  plugins: [
+    [
+      '@vuepress/pwa',
+      {
+        serviceWorker: true,
+        updatePopup: true
+      }
+    ],
+    [
+      'vuepress-plugin-code-copy',
+      {
+        color: '#3eaf7c',
+        backgroundColor: '#3eaf7c'
+      }
+    ]
+  ],
   locales: {
     '/': {
       lang: 'English',
